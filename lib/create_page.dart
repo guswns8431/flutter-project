995e943cb1208ad1a9df1d4a053f539714fd8471
son <son@son-oogong-ui-MacBookPro.local>
2020-05-04 00:58:07 +0900

    갤러리에서 사진 불러오기

--- lib/create_page.dart
+++ lib/create_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
 
 class CreatePage extends StatefulWidget {
   @override
@@ -7,6 +10,9 @@ class CreatePage extends StatefulWidget {
 
 class _CreatePageState extends State<CreatePage> {
   final textEditingController = TextEditingController();
+
+  File _image;
+
   @override
   void dispose(){
     textEditingController.dispose();
@@ -19,7 +25,7 @@ class _CreatePageState extends State<CreatePage> {
       appBar: _buildAppBar(),
       body: _buildBody(),
       floatingActionButton: FloatingActionButton(
-        onPressed: null,
+        onPressed: _getImage,
         child: Icon(Icons.add_a_photo),
       ),
     );
@@ -37,14 +43,24 @@ class _CreatePageState extends State<CreatePage> {
   }
 
   Widget _buildBody(){
-    return Column(
-      children: <Widget>[
-        Text('No Image'),
-        TextField(
-          decoration: InputDecoration(hintText: '내용을 입력하세요'),
-          controller: textEditingController,
-        )
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          _image == null ? Text('No Image') : Image.file(_image),
+          TextField(
+            decoration: InputDecoration(hintText: '내용을 입력하세요'),
+            controller: textEditingController,
+          )
+        ],
+      ),
     );
+  }
+
+  Future _getImage() async {
+    File image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    
+    setState((){
+      _image = image;
+    });
   }
 }
