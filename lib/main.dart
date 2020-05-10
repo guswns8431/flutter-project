2b498b9d7832ec552edc180ef19490aaab9d643c
son <guswns8431@gmail.com>
2020-05-10 23:22:32 +0900

    Theme

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+        primaryColor: Colors.blue,
+        accentColor: Colors.yellow,
       ),
       home: Scaffold(
         appBar: AppBar(
