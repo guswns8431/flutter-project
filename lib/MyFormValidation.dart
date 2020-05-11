bbd19534fcdd30868d65a0472d933176cb01a563
son <guswns8431@gmail.com>
2020-05-11 16:20:34 +0900

    TextField, AlertDialog

--- lib/MyFormValidation.dart
+++ lib/MyFormValidation.dart
@@ -8,6 +8,22 @@ class MyFormValidation extends StatefulWidget {
 
 class _MyFormValidationState extends State<MyFormValidation> {
   final _formKey = GlobalKey<FormState>();
+  FocusNode nameFocusNode;
+
+  final nameController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    nameFocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    nameFocusNode.dispose();
+    nameController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,6 +53,39 @@ class _MyFormValidationState extends State<MyFormValidation> {
                 },
                 child: Text('완료'),
               ),
+            ),
+            TextField(
+              controller: nameController,
+              onChanged: (text){
+                print(text);
+              },
+              focusNode: nameFocusNode,
+              decoration: InputDecoration(
+                hintText: '이름을 입력해 주세요',
+                border: InputBorder.none,
+                labelText: '이름'
+              ),
+              autofocus: true,
+            ),
+            RaisedButton(
+              onPressed: (){
+                FocusScope.of(context).requestFocus(nameFocusNode);
+              },
+              child: Text('포커스'),
+            ),
+            RaisedButton(
+              onPressed: (){
+                print(nameController.text);
+                showDialog(
+                  context: context,
+                  builder: (context){
+                    return AlertDialog(
+                      content: Text(nameController.text),
+                    );
+                  }
+                );
+              },
+              child: Text('TextField값 확인'),
             )
           ],
         )
