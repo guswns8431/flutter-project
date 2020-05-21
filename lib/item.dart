534fc5c18fa0dd2d89f4d1dd0879c59fdcad5d90
son <guswns8431@gmail.com>
2020-05-21 16:14:38 +0900

    대략적인 UI

---
+++ lib/item.dart
@@ -0,0 +1,14 @@
+class Item{
+  final String title;
+  final int price;
+
+  Item(this.title,this.price);
+}
+
+final itemList = [
+  Item("맥북1",2000000),
+  Item("맥북2",3000),
+  Item("맥북3",402000),
+  Item("맥북4",5005000),
+  Item("맥북5",600000),
+];
