bcc9b821640581fbec78e627643b5ebca6a23136
son <guswns8431@gmail.com>
2020-05-29 14:49:05 +0900

    inherited

--- lib/model/item.dart
+++ lib/model/item.dart
@@ -4,6 +4,6 @@ class Item{
   final String title;
   final int price;
 
-  Item({@required this.title,@required this.price}); //TODO: 여기서 required가 필요한 이유
+  Item({@required this.title,@required this.price}); //TODO: 여기서 required가 꼭 필요한건지, 아니면 개발에 조금 더 편의를 위하여 한건지
 
 }
