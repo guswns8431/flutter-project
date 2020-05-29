c5f5cf73ad1a85c1a943ce9d27fb8fe67eb5bf76
son <guswns8431@gmail.com>
2020-05-29 14:37:38 +0900

    item list

---
+++ lib/model/item.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Item{
+  final String title;
+  final int price;
+
+  Item({@required this.title,@required this.price}); //TODO: 여기서 required가 필요한 이유
+
+}
