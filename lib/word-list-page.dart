a1ed3780b1b77039588a83a1d597a9d95bd3c1e2
son <guswns8431@gmail.com>
2020-06-09 21:03:52 +0900

    word-list-page

--- lib/word-list-page.dart
+++ lib/word-list-page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class WordList extends StatefulWidget {
@@ -6,17 +7,68 @@ class WordList extends StatefulWidget {
 }
 
 class _WordListState extends State<WordList> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //TODO: 데이터베이스에서 값을 가져와서 나열해야함
-      body: RaisedButton(
-        child: Text('홈'),
-        onPressed: () {
-          Navigator.pushNamed(context, 'home');
-        },
+      body: ListView(
+        children: <Widget>[
+          Container(
+            height: 500,
+            child: StreamBuilder<QuerySnapshot>(
+              stream: Firestore.instance.collection('wordbook').snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasError) return Text('Error: ${snapshot.error}');
+                switch (snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                    return Text('Loading...');
+                  default:
+                    return ListView(
+                      children: snapshot.data.documents
+                          .map((DocumentSnapshot document) {
+                        return Card(
+                          child: Container(
+                            child: Column(
+                              children: <Widget>[
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Text(
+                                      document['English'],
+                                      style: TextStyle(
+                                        color: Colors.blueGrey,
+                                        fontSize: 17,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Container(
+                                  alignment: Alignment.centerLeft,
+                                  child: Text(
+                                    document['Korean'],
+                                    style: TextStyle(color: Colors.black54),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                    );
+                }
+              },
+            ),
+          ),
+          RaisedButton(
+            child: Text('홈'),
+            onPressed: () {
+              Navigator.pushNamed(context, 'home');
+            },
+          ),
+        ],
       ),
-      );
+    );
   }
 }
